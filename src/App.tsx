1b5f2d1d97948616a3e7a4fa1d1952649a4476b5
MarcosCast <marcosmax.5@gmail.com>
2023-03-14 15:44:05 -0300

    Inserindo useState e useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import './App.scss';
+//import './App.scss';
 
 
 function App() {
+  const [location, setLocation] = useState(false);
+
+  useEffect(()=> {
+    navigator.geolocation.getCurrentPosition((position)=> {
+      console.log(position.coords.latitude, position.coords.longitude);
+      setLocation(true)
+    })
+  }, [])
 
   /* API Key - 543e203878f91a37456c44cbf6cd7197 */ 
   return (
-    <div className="App">
-      <body>
-        
-        
-
-      </body>
-      
-    </div>
+    <Fragment>
+      <h3>Clima (Ex)</h3>
+      <hr />
+      <ul>
+        <li>Temperatura atual: aº</li>
+        <li>Temperatura máxima: aº</li>
+        <li>Temperatura mínima: aº</li>
+        <li>Pressão: a hpa</li>
+        <li>Umidade: a%</li>
+      </ul>
+    </Fragment>  
   );
 }
 
