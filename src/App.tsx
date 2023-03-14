5632fee57d07fbc6d497365dea8826b31e60b340
MarcosCast <marcosmax.5@gmail.com>
2023-03-14 16:38:58 -0300

    Alterando a imagem ico

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.scss';
-
+//import React from 'react';
+//import logo from './logo.svg';
+//import './App.scss';
 
 function App() {
+  /* API Key - 543e203878f91a37456c44cbf6cd7197 */ 
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="search-box">
+          <i className="fa-solid fa-location-dot"></i>
+          <input type="text" placeholder="Digite sua cidade" />
+          <button className="fa-solid fa-magnifying-g"></button>
 
+        </div>
+        
+      </div>      
+    </div>
+  );
 }
 
 export default App;
